0ec7554b1a37023a82b0b3e92fc709e8f730a738
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2023-08-09 02:25:34 +0300

    handle quit, help, EOF

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""command interpreter."""
+
+import cmd
+import json
+
+class HBNBCommand(cmd.Cmd):
+
+	prompt = "(hbnb) "
+	def do_quit(self, line):
+		"""Quit command to exit the program
+		"""
+		return True
+
+	def do_EOF(self, line):
+		"""EOF command to exit the program
+		"""
+		print()
+		return True
+
+	def emptyline(self):
+		"""don't execute anything
+		"""
+		pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
