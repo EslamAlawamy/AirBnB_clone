43e0a7c5fee150341916b32a75f799c35ac1c6e7
elshafae <mohamedelshafae888@gmail.com>
2023-08-11 20:31:48 +0300

    create function

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 
 import cmd
 import json
+from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
 
@@ -22,6 +24,16 @@ class HBNBCommand(cmd.Cmd):
 		"""don't execute anything
 		"""
 		pass
-
+	def do_create(self, cls):
+		"""Creates a new instance of BaseModel"""
+		if cls == "BaseModel":
+			obj = BaseModel()
+			storage.save()
+			print(obj.id)
+		elif cls != "BaseModel":
+			print("** class name missing **")
+		else:
+			print("** class doesn't exist **")
+ 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
