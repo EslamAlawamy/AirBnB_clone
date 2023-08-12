b63aeb93a6723d12e48aaf2f36933b36f38f479b
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-08-12 06:24:26 -0700

    style

--- models/user.py
+++ models/user.py
@@ -2,11 +2,11 @@
 """ User model """
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
-	"""represent User class"""
+    """represent User class"""
 
-
-	email = ""
-	password = ""
-	first_name = ""
-	last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
