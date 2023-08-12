0081890e728d43aae0fd21c3f6040ee3e2b4532a
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-08-12 05:56:36 -0700

    style

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,41 +11,42 @@ from models.review import Review
 
 
 class FileStorage:
-	""" class FileStorage """
+    """ class FileStorage """
 
-	__file_path = "file.json"
-	__objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
-	def all(self):
-		""" returns the dictionary __objects """
-		return FileStorage.__objects
+    def all(self):
+        """ returns the dictionary __objects """
+        return FileStorage.__objects
 
-	def new(self, obj):
-		""" sets in __objects the obj with key <obj class name>.id """
-		key = obj.__class__.__name__ + "." + obj.id
-		FileStorage.__objects[key] = obj
+    def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id """
+        key = obj.__class__.__name__ + "." + obj.id
+        FileStorage.__objects[key] = obj
 
-	def save(self):
-		""" serializes __objects to the JSON file (path: __file_path) """
-		dictionary = {}
-		for key, value in FileStorage.__objects.items():
-        		dictionary[key] = value.to_dict()
+    def save(self):
+        """ serializes __objects to the JSON file (path: __file_path) """
+        dictionary = {}
+        for key, value in FileStorage.__objects.items():
+            dictionary[key] = value.to_dict()
 
-		with open(FileStorage.__file_path, 'w', encoding="utf-8") as f:
-           		json.dump(dictionary, f, indent=2)
+        with open(FileStorage.__file_path, 'w', encoding="utf-8") as f:
+            json.dump(dictionary, f, indent=2)
 
-	def reload(self):
-		"""
-		deserializes the JSON file
-		to __objects (only if the
-		JSON file (__file_path)
-		"""
-		models = {'User': User, 'BaseModel': BaseModel, 'State': State, 'City': City,
-                        'Amenity': Amenity, 'Place': Place, 'Review': Review}
-		with open(self.__file_path, "r", encoding='utf-8') as f:
-			json_objs = json.load(f)
+    def reload(self):
+        """
+        deserializes the JSON file
+        to __objects (only if the
+        JSON file (__file_path)
+        """
+        models = {'User': User, 'BaseModel': BaseModel, 'State': State,
+                  'City': City, 'Amenity': Amenity, 'Place': Place,
+                  'Review': Review}
+        with open(self.__file_path, "r", encoding='utf-8') as f:
+            json_objs = json.load(f)
 
-		for key, value in json_objs.items():
-			for model, cls in models.items():
-				if value["__class__"] == model:
-					self.__objects[key] = cls(**value)
+        for key, value in json_objs.items():
+            for model, cls in models.items():
+                if value["__class__"] == model:
+                    self.__objects[key] = cls(**value)
